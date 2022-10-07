095aea7a27181d1abb0e770a8eecbc5ff53f5fb3
Pietro Weg <pietro.sera@compasso.com.br>
2022-10-07 16:16:31 -0300

    :sparkles: feat: componente summary desenvolvido

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Header/Summary";
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 };
