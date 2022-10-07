9951db4d89a8b08d22af499233780c591e0c6592
Pietro Weg <pietro.sera@compasso.com.br>
2022-10-07 11:22:46 -0300

    :tada: init: commit inicial
    arquitetura definida, implementado o global-styles e default-theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+} as const
+
+// o "as const" diz ao typescript que esse valor nunca irá mudar, assim, o ts consegue dizer o hexadecimal ao mostrar o tipo da propriedade;
+// sem o "as const", o tipagem viria apenas como string.
