7cc485bb47d15b18484897ebdb173b7990150229
Pietro Weg <pietro.sera@compasso.com.br>
2022-10-07 15:51:12 -0300

    :sparkles: feat: header desenvolvido

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionButton
+} from "./styles";
+
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+};
