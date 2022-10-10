37bc2dccf28186a7e090cd85c2dfa27a219afa9f
Pietro Weg <pietro.sera@compasso.com.br>
2022-10-10 19:35:11 -0300

    :construction: feat: criando modal acessível com radix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,8 @@ import {
   NewTransactionButton
 } from "./styles";
 
+import * as Dialog from '@radix-ui/react-dialog'
+
 import logoImg from '../../assets/logo.svg'
 
 export function Header() {
@@ -12,8 +14,29 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
 };
+
+
+// Root: Contains all the parts of a dialog. Precisa engolbar todo o modal;
+// Trigger asChild: Botão para abrir modal. asChild - propriedade que aproveita o elemento dentro da tag como botao para abrir;
+// Portal: forma elegante de renderizar um elemento filho, dentro de outro lugar na DOM (a parte da aplicaçao). When used, portals your overlay and content parts into the body. Cria conteúdo fora do contexto da aplicação (root);
+// Overlay: A layer that covers the inert portion of the view when the dialog is open.
+// Contetn: Conteúdo principal do modal. Possuí title acessível (anunciado quando o modal é aberto, pode ser hidden) e description (decricao opcional acessivel, com opcao de hidden).
