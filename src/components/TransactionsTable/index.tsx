b8d2b9904d40ed6a5a3e2e772811cebd6304c99f
Pietro Weg <pietro.sera@compasso.com.br>
2022-10-16 20:54:27 -0300

    :sparkles: feat: requisitando transacoes http com fetch

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { SearchForm } from "../SearchForm";
 import {
   TransactionsContainer,
@@ -5,35 +6,52 @@ import {
   PriceHighLight
 } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = await response.json();
+
+    setTransactions(data);
+  };
+
+  // useEffect não aceita async/await https://www.designcise.com/web/tutorial/why-cant-react-useeffect-callback-be-async
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+
   return (
     <TransactionsContainer>
       <SearchForm />
       <Table>
         <tbody>
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighLight variant='income'>
-                R$ 12.000,00
-              </PriceHighLight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Hamburguer</td>
-            <td>
-              <PriceHighLight variant='outcome'>
-                - R$ 12.000,00
-              </PriceHighLight>
-            </td>
-            <td>Alimentação</td>
-            <td>10/04/2022</td>
-          </tr>
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighLight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighLight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </Table>
-    </TransactionsContainer>
+    </TransactionsContainer >
   )
 }
--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -11,7 +11,7 @@ export const Table = styled.table`
   width: 100%;
   border-collapse: separate; // propriedade específica de tabela
   border-spacing: 0 0.5rem; // propriedade específica de tabela
-  /* margin-top: 1.5rem; */
+  margin-top: 1.5rem;
 
   td {
     padding: 1.25rem 2rem;
