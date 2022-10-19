78108ca13c91d39b89aaf8aa9a653b25e3465e71
Pietro Weg <pietro.sera@compasso.com.br>
2022-10-18 23:21:27 -0300

    :sparkles: feat: criando hook para calculo de transacoes

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,30 +7,10 @@ import {
   SummaryContainer
 } from "./styles";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  // Reduzir meu array de transactions para essa estrutura de dados
-  // {income: 0, outcome: 0, total: income - outcome}
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-      return acc;
-    },
-    {
-      // acc
-      income: 0,
-      outcome: 0,
-      total: 0
-    }
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
